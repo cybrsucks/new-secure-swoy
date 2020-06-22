c262542ebd354a0f3f509f94969538ca3fc87bae
Celestares <orionraysher65@gmail.com>
2020-06-22 14:43:41 +0800

    Home

--- app.py
+++ app.py
@@ -6,7 +6,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    test_list = [
+        {"name": "Green Tea", "image": "bayek.jpg"},
+        {"name": "Puck you", "image": "puck you.png"}
+    ]
+
+    return render_template("home.html", test_list=test_list)
 
 
 if __name__ == "__main__":
