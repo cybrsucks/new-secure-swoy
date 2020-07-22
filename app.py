1ea5716dc9c797b288bf2c30b7b29ec2fc8669dd
Leviathan <cybrs36@gmail.com>
2020-07-22 17:39:25 +0800

    fixed transaction-details-container

--- app.py
+++ app.py
@@ -707,27 +707,6 @@ def view_profile():
 
     username_form = ChangeLoggedInUserUsernameForm()
     password_form = ChangeLoggedInUserPasswordForm()
-
-    # if request.method == "POST" and username_form.validate_on_submit():
-    #     with sqlite3.connect("swoy.db") as conn:
-    #         cursor = conn.cursor()
-    #         new_username = username_form.new_username.data
-    #         cursor.execute(f"UPDATE user SET username = '{new_username}' WHERE user_id = '{user_id}'")
-    #         conn.commit()
-    #     return redirect(url_for('view_profile'))
-    #
-    # elif request.method == "POST" and password_form.validate_on_submit():
-    #     with sqlite3.connect("swoy.db") as conn:
-    #         cursor = conn.cursor()
-    #         current_password_from_db = cursor.execute(f"SELECT password WHERE user_id = '{user_id}'")
-    #         if current_password_from_db != password_form.current_pwd.data:
-    #             print("The current password was incorrect. Your password cannot be changed ta this time.")
-    #         else:
-    #             new_password = password_form.new_pwd.data
-    #             cursor.execute(f"UPDATE user SET password = '{new_password}' WHERE user_id = '{user_id}'")
-    #             conn.commit()
-    #         return redirect(url_for('home'))
-
     return render_template("profile.html", username_form=username_form, password_error=password_error, password_form=password_form, user_account=user_account, cart_item_count=cart_item_count)
 
 
@@ -768,39 +747,8 @@ def change_password():
         return redirect(url_for("home"))
 
 
-# @app.route("/password", methods=["GET", "POST"])
-# def password():
-#     try:
-#         user_id = request.args["id"]
-#         with sqlite3.connect("swoy.db") as conn:
-#             cursor = conn.cursor()
-#             cursor.execute(f"SELECT * FROM user WHERE user_id = '{user_id}'")
-#             user_account = cursor.fetchone()
-#             cursor.execute(f"SELECT cart_items FROM cart WHERE user_id = '{user_id}'")
-#             cart_items = cursor.fetchone()
-#             if cart_items:
-#                 cart_item_count = len(eval(cart_items[0]))
-#             else:
-#                 cart_item_count = 0
-#     except:
-#         user_account = None
-#         cart_item_count = 0
-#
-#     password_form = ChangeLoggedInUserPasswordForm()
-#     if request.method == "POST" and password_form.validate_on_submit():
-#         with sqlite3.connect("swoy.db") as conn:
-#             cursor = conn.cursor()
-#             current_password_from_db = cursor.execute(f"SELECT password WHERE user_id = '{user_id}'")
-#             if current_password_from_db != password_form.current_pwd.data:
-#                 print("The current password was incorrect. Your password cannot be changed ta this time.")
-#             else:
-#                 new_password = password_form.new_pwd.data
-#                 cursor.execute(f"UPDATE user SET password = '{new_password}' WHERE user_id = '{user_id}'")
-#                 conn.commit()
-#             return redirect(url_for('home'))
-#
-#     return render_template("change_password.html", password_form=password_form,  user_account=user_account, cart_item_count=cart_item_count)
-#
+@app.route("/order_history", methods=["GET", "POST"])
+
 
 @app.route("/pw")
 def pw():
